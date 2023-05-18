4391ad66e6a5ef25efdaf944759161085c3948f6
MUHAMMAD ADHIEM WICAKSANA <102276858+madhiemw@users.noreply.github.com>
2023-05-18 23:31:31 +0700

    Update constants.go

--- constants/constants.go
+++ constants/constants.go
@@ -6,7 +6,7 @@ const (
 	Male       = "Male"
 	Female     = "Female"
 	SECRET_JWT = "secret"
-	SENDGRID_API_KEY = "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"
+	SENDGRID_API_KEY = ""
 	SEND_FROM_NAME = "Inventron_Support"
 	SEND_FROM_ADDRESS = "madhiemw@gmail.com"
 )
