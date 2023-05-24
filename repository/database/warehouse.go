ede12eb5ae2e0df0af276d199cde383d30d48cae
Alfian Subaru Putra <subaruputra13@gmail.com>
2023-05-24 20:01:20 +0700

    Fitur get all warehouse by status

--- repository/database/warehouse.go
+++ repository/database/warehouse.go
@@ -5,26 +5,29 @@ import (
 	"Capstone/models"
 )
 
+// get all warehouse query database
 func GetAllWarehouses() (warehouse []models.Warehouse, err error) {
 	if err := config.DB.Find(&warehouse).Error; err != nil {
 		return nil, err
 	}
+
 	return warehouse, nil
 }
 
-func GetWarehouseByid(id uint) (warehouse *models.Warehouse, err error) {
-	warehouse.ID = id
-	if err := config.DB.Where("id = ?", id).First(&warehouse).Error; err != nil {
+func GetAllAvailableWarehouses(status string) (warehouse []models.Warehouse, err error) {
+	if err := config.DB.Where("status = ? ", status).Find(&warehouse).Error; err != nil {
 		return nil, err
 	}
 
 	return warehouse, nil
 }
 
+// delete warehouse query database
 func DeleteWarehouse(warehouse *models.Warehouse) error {
 	if err := config.DB.Delete(warehouse).Error; err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -33,6 +36,7 @@ func CreateWarehouse(warehouse *models.Warehouse) error {
 	if err := config.DB.Create(warehouse).Error; err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -41,20 +45,15 @@ func UpdateWarehouse(warehouse *models.Warehouse) error {
 	if err := config.DB.Model(&warehouse).Updates(&warehouse).Error; err != nil {
 		return err
 	}
+
 	return nil
 }
 
-// get all warehouse query database
-func GetWarehouses() (warehouses []models.Warehouse, err error) {
-	if err = config.DB.Model(&models.Warehouse{}).Find(&warehouses).Error; err != nil {
-		return
-	}
-	return
-}
-
+// get warehouse by id query database
 func GetWarehouseByID(id uint64) (warehouse *models.Warehouse, err error) {
 	if err = config.DB.Where("id = ?", id).First(&warehouse).Error; err != nil {
 		return nil, err
 	}
+
 	return warehouse, nil
 }
