b63ecf35eeafe3e9a0a426dff323a3fb768f5219
Alfian Subaru Putra <108457065+Subaruputra13@users.noreply.github.com>
2023-06-14 06:31:29 +0700

    testing ci/cd

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"Capstone/config"
 	"Capstone/middleware"
 	"Capstone/routes"
+	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
 func main() {
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Fatal("failed to load .env")
-	// }
+	err := godotenv.Load("deploy.env")
+	if err != nil {
+		log.Fatal("failed to load .env")
+	}
 
 	db := config.InitDB()
 	e := echo.New()
